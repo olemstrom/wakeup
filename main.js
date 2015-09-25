c8b96646eb73ebe6aee2150611a75124066fa10e
= <=>
2015-09-25 17:22:43 +0300

    add time functions

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 var WakeUp = (function(){
-	var timeFormat = "hh:mm:ss DD.MM.YYYY";
+	var formating = {
+		timeFormat : "HH:mm:ss",
+		inputTimeFormat : "HH:mm"
+	}
+
 	var DOM = {
-		currentTime : document.querySelector("#current-time")
+		currentTime : document.querySelector("#current-time"),
+		alarmTime   : document.querySelector("#alarm-time"),
+		setupTime   : document.querySelector("[name=time]"),
+		updateSetup : document.querySelector(".setup button")
+	}
+
+	var conf = {
+		targetTime: moment(),
+		youtubeID: ""
 	}
 
 	function update() {
 		var now = moment();
-
+		DOM.currentTime.innerHTML = now.format(formating.timeFormat);
+		DOM.alarmTime.innerHTML = conf.targetTime.format(formating.timeFormat);
+		
 	}
+
+	function configure() {
+		conf.targetTime = moment(DOM.setupTime.value, formating.inputTimeFormat);
+	}
+
+	function bindEvents() {
+		DOM.updateSetup.addEventListener("click", function(event){
+			conf.targetTime = moment(DOM.setupTime.value, formating.inputTimeFormat);
+			console.log(conf);
+		});
+	}
+
 	function start() {
+		DOM.setupTime.value = conf.targetTime.format(formating.inputTimeFormat);
+
 		window.setInterval(function(){
 			update();
 		}, 1000) // update everything every second
